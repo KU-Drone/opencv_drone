bdbf96dafd40c1b11567397704e478458877b3b1
batuogkoc <bgunduz21@ku.edu.tr>
2022-07-18 17:43:48 +0300

    same as previous commit

--- scripts/startsitl.sh
+++ scripts/startsitl.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-cd ../ardupilot/ArduCopter && sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# cd ../ardupilot/ArduCopter && sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+# cd ../ardupilot/ArduCopter
+cd $SCRIPT_DIR/../ArduCopter
+pwd
+sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+# x=1
+# while [$x -e 1]
+# do
+# done    
